7f9eaae9a5df12ea3c41196c10f07507da531364
inhwanK <inhan1009@naver.com>
2025-02-28 01:52:41 +0900

    feat(core): service 로직 추가 #9
    리마인더 체크 로직 추가
    데이터베이스 정보 조회 메서드 수정

---
+++ reminder-core/src/main/java/ind/venture/remindercore/service/NotionDatabaseService.java
@@ -0,0 +1,53 @@
+package ind.venture.remindercore.service;
+
+import ind.venture.remindercore.domain.Database;
+import ind.venture.remindercore.domain.filter.DateFilter;
+import ind.venture.remindercore.domain.filter.PropertyFilter;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class NotionDatabaseService {
+
+    private final WebClient notionClient;
+    private static final String DATABASE_URI = "/databases/";
+
+    public NotionDatabaseService(WebClient notionClient) {
+        this.notionClient = notionClient;
+    }
+
+    public Mono<Database> retrieveDatabaseInfo(String apiKey, String databaseId) {
+        return notionClient.get()
+                .uri(DATABASE_URI + databaseId)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + apiKey)
+                .retrieve()
+                .bodyToMono(Database.class);
+    }
+
+    public Mono<Boolean> checkHasReminderProperty(String apiKey, String databaseId) {
+        return notionClient.get()
+                .uri(DATABASE_URI + databaseId)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + apiKey)
+                .retrieve()
+                .bodyToMono(Database.class)
+                .map(Database::isReminder);
+    }
+
+    public Mono<Boolean> findReminderPage(String apiKey, String databaseId) {
+        DateFilter dateFilter = DateFilter.builder()
+                .isNotEmpty(true)
+                .build();
+
+        return notionClient.post()
+                .uri(DATABASE_URI + databaseId + "/query")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer " + apiKey)
+                .bodyValue(new PropertyFilter("리마인더", dateFilter))
+                .retrieve()
+                .bodyToMono(Boolean.class);
+    }
+}
