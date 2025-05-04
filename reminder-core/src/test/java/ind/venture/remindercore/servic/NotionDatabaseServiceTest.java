5895310605ac92dcea40b402513b913805dc5110
inhwanK <inhan1009@naver.com>
2025-05-05 04:20:18 +0900

    test(core): NotionDatabaseServiceTest 생성

---
+++ reminder-core/src/test/java/ind/venture/remindercore/servic/NotionDatabaseServiceTest.java
@@ -0,0 +1,36 @@
+package ind.venture.remindercore.servic;
+
+import ind.venture.remindercore.service.NotionDatabaseService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class NotionDatabaseServiceTest {
+
+    private NotionDatabaseService notionDatabaseService;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    void getDatabaseInfo_Success() {
+        Assertions.fail("미구현");
+    }
+
+    @Test
+    void getDatabaseInfo_Exception() {
+        Assertions.fail("미구현");
+    }
+
+    @Test
+    void findReminderPageSuccess() {
+        Assertions.fail("미구현");
+    }
+
+    @Test
+    void findReminderPageException() {
+        Assertions.fail("미구현");
+    }
+}
