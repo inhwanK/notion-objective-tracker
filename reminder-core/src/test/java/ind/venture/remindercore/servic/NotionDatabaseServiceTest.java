22ec60aeb810d4371a3cbc403d96c8cf606c6a10
inhwanK <inhan1009@naver.com>
2025-05-06 03:43:16 +0900

    test(core): 테스트 코드 추가
    getDatabaseInfo, checkIsReminderDatabase 테스트 코드 추가

--- reminder-core/src/test/java/ind/venture/remindercore/servic/NotionDatabaseServiceTest.java
+++ reminder-core/src/test/java/ind/venture/remindercore/servic/NotionDatabaseServiceTest.java
@@ -4,20 +4,20 @@ import ind.venture.remindercore.config.NotionWebClient;
 import ind.venture.remindercore.domain.Database;
 import ind.venture.remindercore.domain.property.DatabaseProperty;
 import ind.venture.remindercore.service.NotionDatabaseService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class NotionDatabaseServiceTest {
@@ -34,6 +34,44 @@ public class NotionDatabaseServiceTest {
     }
 
     @Test
+    void getDatabaseInfo_Success() {
+        Database db = new Database(
+                "db-id",
+                "https://notion.so/db-id",
+                "https://notion.so/public/db-id",
+                "2024-01-01T00:00:00.000Z",
+                "2024-01-02T00:00:00.000Z",
+                true,
+                false,
+                "req-id",
+                false,
+                Map.of("title", new DatabaseProperty("title", "Title", "text"))
+        );
+
+        when(notionWebClient.fetchDatabase("token", "db-id"))
+                .thenReturn(Mono.just(db));
+
+        StepVerifier.create(notionDatabaseService.getDatabaseInfo("token", "db-id"))
+                .expectNext(db)
+                .verifyComplete();
+    }
+
+    @Test
+    void getDatabaseInfo_400Error() {
+        WebClientResponseException badRequestException = WebClientResponseException.create(
+                400, "Bad Request", null, null, null);
+
+        when(notionWebClient.fetchDatabase("token", "db-id"))
+                .thenReturn(Mono.error(badRequestException));
+
+        StepVerifier.create(notionDatabaseService.getDatabaseInfo("token", "db-id"))
+                .expectErrorMatches(throwable ->
+                        throwable instanceof WebClientResponseException ex &&
+                        ex.getStatusCode() == HttpStatus.BAD_REQUEST)
+                .verify();
+    }
+
+    @Test
     void checkIsReminderDatabase_ShouldReturnTrue() {
         Database db = new Database("db123", Map.of("리마인더", new DatabaseProperty("1", "리마인더", "date")));
 
@@ -46,22 +84,16 @@ public class NotionDatabaseServiceTest {
     }
 
     @Test
-    void getDatabaseInfo_Success() {
-        Assertions.fail("미구현");
+    void checkIsReminderDatabase_ShouldReturnFalse() {
+        Database db = new Database("db123", Map.of("리마", new DatabaseProperty("1", "리마", "date")));
+
+        when(notionWebClient.fetchDatabase("token", "db123"))
+                .thenReturn(Mono.just(db));
+
+        StepVerifier.create(notionDatabaseService.checkIsReminderDatabase("token", "db123"))
+                .expectNext(false)
+                .verifyComplete();
     }
 
-    @Test
-    void getDatabaseInfo_Exception() {
-        Assertions.fail("미구현");
-    }
 
-    @Test
-    void findReminderPageSuccess() {
-        Assertions.fail("미구현");
-    }
-
-    @Test
-    void findReminderPageException() {
-        Assertions.fail("미구현");
-    }
 }
