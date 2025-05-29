329ffd53ada6418c6f244136fb4f03be1ac3d56e
inhwanK <inhan1009@naver.com>
2025-05-29 23:56:52 +0900

    feat(notion): notion webhooks 객체 클래스 설계
    PagePropertiesUpdatedHook 추가
    AccessibleBy, Author, Data, Entity, IdTypePair, Parent 추가

---
+++ objective-notion/src/main/java/ind/venture/objectivenotion/model/webhooks/common/Data.java
@@ -0,0 +1,17 @@
+package ind.venture.objectivenotion.model.webhooks.common;
+
+import java.util.List;
+
+public class Data {
+    private Parent parent;
+    // 훅의 종류에 따라 타입이 바뀜
+    private List<String> updatedProperties;
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public List<String> getUpdatedProperties() {
+        return updatedProperties;
+    }
+}
---
+++ objective-notion/src/main/java/ind/venture/objectivenotion/model/webhooks/common/IdTypePair.java
@@ -0,0 +1,14 @@
+package ind.venture.objectivenotion.model.webhooks.common;
+
+public class IdTypePair {
+    private String id;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
