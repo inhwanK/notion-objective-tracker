87e8ac11e9b4f82caa001c6e75b6db651e8e614d
inhwanK <inhan1009@naver.com>
2025-07-14 22:03:21 +0900

    update: ObjectiveService 역할 분리
    NotionObjectiveManager 추가
    SubObjectiveGenerator 추가

--- objective-notion-service/src/main/java/ind/venture/objectivenotionservice/service/ObjectiveService.java
+++ objective-notion-service/src/main/java/ind/venture/objectivenotionservice/service/ObjectiveService.java
@@ -1,75 +1,24 @@
 package ind.venture.objectivenotionservice.service;
 
-import ind.venture.objectivenotion.model.page.Page;
-import ind.venture.objectivenotion.model.page.property.PageProperty;
-import ind.venture.objectivenotion.model.page.property.RelationProperty;
-import ind.venture.objectivenotion.model.page.type.Relation;
 import ind.venture.objectivenotion.model.webhooks.NotionWebhookEvent;
-import ind.venture.objectivenotionservice.client.NotionPageClient;
+import ind.venture.objectivenotionservice.support.NotionObjectiveManager;
+import ind.venture.objectivenotionservice.support.SubObjectiveGenerator;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class ObjectiveService {
-    private final NotionPageClient notionPageClient;
-
-    public ObjectiveService(NotionPageClient notionPageClient) {
-        this.notionPageClient = notionPageClient;
-    }
+    private final NotionObjectiveManager notionManager;
+    private final SubObjectiveGenerator subObjectiveGenerator;
 
     public Mono<Void> createSubObjective(String apiKey, NotionWebhookEvent event) {
-        return getCreatableSubObjectivePage(apiKey, event)
-                .flatMap(page -> {
-                    List<Relation> relations = extractSubRelations(page);
-                    // 기존 하위 목표 제거
-                    return Flux.fromIterable(relations)
-                            .flatMap(relation -> notionPageClient.deletePage(apiKey, relation.getId()))
-                            .then();
-                });
-    }
 
-    private List<Relation> extractSubRelations(Page page) {
-        PageProperty prop = page.getProperties().get("하위 항목");
-        if (prop instanceof RelationProperty) {
-            List<Relation> relations = ((RelationProperty) prop).getRelation();
-            log.info("relations: {}", relations);
-            return (relations != null) ? relations : Collections.emptyList();
-        }
-        return Collections.emptyList();
-    }
-
-
-    public Mono<Page> getCreatableSubObjectivePage(String apiKey, NotionWebhookEvent event) {
-        String pageId = event.getEntity().getId();
-        List<String> properties = event.getData().getUpdatedProperties();
-
-        return notionPageClient.fetchPage(apiKey, pageId)
-                .doOnNext(notionPage -> log.info("notionPage {}", notionPage))
-                .filter(page -> validateRelation(page) && validateSubObjectiveCreatedAt(page.getProperties(), properties))
-                .switchIfEmpty(Mono.error(new IllegalStateException("하위 목표 생성 조건이 아닙니다.")));
-    }
-
-    private boolean validateRelation(Page page) {
-        Map<String, PageProperty> props = page.getProperties();
-        PageProperty sub = props.get("하위 항목");
-        PageProperty parent = props.get("상위 항목");
-
-        return sub != null
-                && parent != null
-                && "relation".equals(sub.getType())
-                && "relation".equals(parent.getType());
-    }
-
-    private boolean validateSubObjectiveCreatedAt(Map<String, PageProperty> properties, List<String> updatedProperties) {
-        PageProperty property = properties.get("하위 목표 생성 시간");
-        return property != null && updatedProperties.contains(property.getId());
+        return notionManager.getCreatableSubObjectivePage(apiKey, event)
+                .flatMap(page -> notionManager.deleteAllSubObjectives(apiKey, page));
     }
 }
---
+++ objective-notion-service/src/main/java/ind/venture/objectivenotionservice/support/SubObjectiveGenerator.java
@@ -0,0 +1,11 @@
+package ind.venture.objectivenotionservice.support;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class SubObjectiveGenerator {
+//    private final ChatClient chatClient;
+
+}
